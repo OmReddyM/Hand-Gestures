f2e2d007a4f05e20eb13d18540fa13022e9cb7df
OmReddyM <90816746+OmReddyM@users.noreply.github.com>
2021-12-25 16:58:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-"https://teachablemachine.withgoogle.com/models/MMo5cPdLf/"
+prediction = "";
+Webcam.set({
+    width:350, height: 300, image_format: 'png', png_quality: 90
+})
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function takeSnapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("snapshot").innerHTML = "<img id='capturedImage' src='" + data_uri +"'>";
+    })
+}
+
+console.log('ml5 version = ', ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/MMo5cPdLf/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded");
+}
+
+function speak() {
+    data = "The prediction is " + prediction;
+    utterance = new SpeechSynthesisUtterance(data);
+    window.speechSynthesis.speak(utterance);
+}
